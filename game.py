7788fbc0918a767a34922c4297b0f176c495a947
Rihler <ermakoffsergey@gmail.com>
2023-10-06 09:40:03 +0300

    game

--- game.py
+++ game.py
@@ -1,6 +1,22 @@
+def checkWinner(lst, sym):
+    winner = 0
+    win_lst = [["A1", "A2", "A3"],
+               ["B1", "B2", "B3"],
+               ["C1", "C2", "C3"],
+               ["A1", "B2", "C3"],
+               ["A3", "B2", "C1"],
+               ["A1","B1", "C1"],
+               ["A2","B2", "C2"],
+               ["A3", "B3", "C3"]]
+    for i in range(8):
+        if lst[win_lst[i][0]] ==sym and lst[win_lst[i][1]] == sym and lst[win_lst[i][2]] == sym:
+            winner = lst[win_lst[i][0]]
+            break
+    return winner
+
 class Field(object):
     def __init__(self):
-        self.field = [["|A1|", "|A2|", "|A3|"], ["|B1|", "|B2|", "|B3|"], ["|C1|", "|C2|", "|C3|"]]
+        self.field = [["|A1 |", "|A2 |", "|A3 |"], ["|B1 |", "|B2 |", "|B3 |"], ["|C1 |", "|C2 |", "|C3 |"]]
         self.cords = {
             "A1": [0, 0],
             "A2": [0, 1],
@@ -25,11 +41,28 @@ class Field(object):
         }
 
     def print_field(self):
-        print('-' * 14)
+        print('-' * 17)
         for i in range(3):
             for j in range(3):
                 print(self.field[i][j], end=" ")
-            print("\n" + '-' * 14)
+            print("\n" + '-' * 17)
+
+    def makeMove(self, sym, x):
+        while x not in self.sym.keys():
+            x = input("Такой координаты нет, введите те, которые доступны на поле: ")
+        if self.sym[x] != 'O' and self.sym[x] !="X":
+            pass
+        else:
+            while self.sym[x] == 'O' or self.sym[x] == "X":
+                x = input("Данная клетка занята, выберите другую: ")
+                while x not in self.sym.keys():
+                    x = input("Такой координаты нет, введите те, которые доступны на поле: ")
+        self.sym[x] = sym
+        x1 = self.cords[x][0]
+        y1 = self.cords[x][1]
+        self.field[x1][y1] = f"| {sym} |"
+        winner = checkWinner(self.sym, sym)
+        return winner
 
 def change_sym(name):
     print(f"Выбор символа для {name}")
@@ -40,19 +73,22 @@ def change_sym(name):
         syms = input("1. X\n2. O\n")
     return "X" if syms == "1" else "O"
 
-def checkWinner(lst, sym):
-    winner = 0
-    win_lst = [["A1", "A2", "A3"],
-               ["B1", "B2", "B3"],
-               ["C1", "C2", "C3"]
-               ["A1", "B2", "C3"],
-               ["A3", "B2", "C1"],
-               ["A1","B1", "C1"],
-               ["A2","B2", "C2"],
-               ["A3", "B3", "C3"]]
-    for i in range(8):
-        if lst[win_lst[i][0]] == lst[win_lst[i][1]] == lst[win_lst[i][2]]\
-              and lst[win_lst[i][0]] != 0:
-            winner = lst[win_lst[i][0]]
-            break
-    return winner
+def game(name1, name2):
+    field = Field()
+    field.print_field()
+    player_syms ={}
+    syms1= change_sym(name1)
+    syms2 = 'O' if syms1 == "X" else "X"
+    player_syms[syms1] = name1
+    player_syms[syms2] = name2
+    win = 0
+    while not(win):
+        for i in player_syms:
+            currentPlayer = i
+            print(f"Ходит - {player_syms[i]}")
+            field.print_field()
+            if field.makeMove(currentPlayer, input("Введите координату клетки: ")) != 0:
+                print(f"Победитель - {player_syms[currentPlayer]}")
+                win = 1
+                break
+game("Sergey", "Hoyus")
